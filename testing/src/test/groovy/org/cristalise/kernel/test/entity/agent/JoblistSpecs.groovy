dcaedf485ff83131ec5edd011addc4e92f6cbf95
Zsolt Kovacs <zs.myth@gmail.com>
2015-10-07 15:29:33 +0200

    Role override test

--- testing/src/test/groovy/org/cristalise/kernel/test/entity/agent/JoblistSpecs.groovy
+++ testing/src/test/groovy/org/cristalise/kernel/test/entity/agent/JoblistSpecs.groovy
@@ -20,6 +20,8 @@
  */
 package org.cristalise.kernel.test.entity.agent
 
+import org.cristalise.dsl.entity.role.RoleBuilder
+import org.cristalise.dsl.lifecycle.stateMachine.StateMachineBuilder
 import org.cristalise.dsl.test.entity.agent.AgentTestBuilder
 import org.cristalise.dsl.test.entity.item.ItemTestBuilder
 import org.cristalise.kernel.entity.agent.JobList
@@ -60,5 +62,49 @@ class JoblistSpecs extends Specification implements CristalTestSetup {
         jobList
         jobList[0]
         jobList[1]
+    }
+
+    def 'StateMachine Transition can override Role specified in Actitiy'() {
+        when:
+        StateMachineBuilder.create("testing", "RoleOverrideSM", 0) {
+            State(id: "0", name: "Waiting")
+            State(id: "1", name: "Started")
+            State(id: "2", name: "Finished", proceeds: "true")
+
+            Transition(id:"1", name:"Start",    origin:"0", target:"1", reservation:"set")
+            Transition(id:"2", name:"Complete", origin:"1", target:"2", reservation:"clear") {
+                Outcome(name:"\${SchemaType}", version:"\${SchemaVersion}")
+                Script( name:"\${ScriptName}", version:"\${ScriptVersion}")
+            }
+            Transition(id:"3", name:"Timeout", origin:"0", target:"0", roleOverride: '${RoleOverride}') {
+                Outcome(name:"Errors", version: "0")
+            }
+        }
+
+        RoleBuilder.create { Role(name: 'toto') }
+
+        AgentTestBuilder timeoutAgent = AgentTestBuilder.create(name: "TimeoutManager") {
+            Roles {
+                Role(name: 'Timeout', jobList: true)
+            }
+        }
+
+        ItemTestBuilder.create(name: "dummyItem", folder: "testing") {
+            Workflow {
+                EA('EA1') {
+                    Property('Agent Role'    : "toto")
+                    Property(RoleOverride    : "Timeout")
+                    Property(StateMachineName: "RoleOverrideSM")
+                }
+            }
+        }
+
+        //some wait is needed until JobPusher thread finishes
+        Thread.sleep(500)
+        def jobList = new JobList(timeoutAgent.agent, null)
+
+        then:
+        jobList
+        jobList[0]
     }
 }
