30c835b10c8a8d4d538ff7dc441f66c08a9b0e6f
Zsolt Kovacs <zs.myth@gmail.com>
2017-01-12 22:07:32 +0100

    JooqOutcome tests

---
+++ jooqdb/src/test/java/org/cristalise/storage/jooqdb/JooqOutcomeTest.java
@@ -0,0 +1,50 @@
+package org.cristalise.storage.jooqdb;
+
+import static org.jooq.impl.DSL.using;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.UUID;
+
+import org.cristalise.kernel.persistency.outcome.Outcome;
+import org.cristalise.kernel.persistency.outcome.Schema;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JooqOutcomeTest {
+    DSLContext context;
+    UUID       uuid = UUID.randomUUID();
+    Outcome    outcome;
+
+    JooqOutcome jooq;
+
+    @Before
+    public void before() throws Exception {
+        String userName = "sa";
+        String password = "sa";
+        String url      = "jdbc:h2:mem:";
+
+        Connection conn = DriverManager.getConnection(url, userName, password);
+        context = using(conn, SQLDialect.H2);
+
+        jooq = new JooqOutcome();
+        jooq.createTables(context);
+
+        outcome = new Outcome(1, "<xml/>", new Schema("SchemaName", 0, "<xs:schema/>"));
+        jooq.put(context, uuid, outcome);
+    }
+
+    @Test
+    public void fetchOutcome() throws Exception {
+        Outcome outcomePrime = jooq.fetch(context, uuid, "SchemaName", 0, 1);
+        assert outcomePrime != null;
+        assert "<xml/>".equals(outcomePrime.getData());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void cannotUpdateOutcome() throws Exception {
+        jooq.put(context, uuid, outcome);
+    }
+}
