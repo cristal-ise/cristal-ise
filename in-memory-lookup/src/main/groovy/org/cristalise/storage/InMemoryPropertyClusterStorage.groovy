e61a2220977c924d1e20fa357c83c8f77db55f6b
Zsolt Kovacs <zs.myth@gmail.com>
2017-08-17 19:07:45 +0200

    Implementing ClusterType changes - #cristalise/kernel#148

--- in-memory-lookup/src/main/groovy/org/cristalise/storage/InMemoryPropertyClusterStorage.groovy
+++ in-memory-lookup/src/main/groovy/org/cristalise/storage/InMemoryPropertyClusterStorage.groovy
@@ -1,20 +1,19 @@
 package org.cristalise.storage
 
-import groovy.transform.CompileStatic
-
-import org.cristalise.kernel.lookup.ItemPath
 import org.cristalise.kernel.persistency.ClusterStorage
-import org.cristalise.kernel.property.Property
+import org.cristalise.kernel.persistency.ClusterType
 import org.cristalise.lookup.lite.InMemoryLookupManager
+
+import groovy.transform.CompileStatic
 
 @CompileStatic
 class InMemoryPropertyClusterStorage extends ClusterStorage {
     @Delegate InMemoryLookupManager lookup = InMemoryLookupManager.instance
 
     @Override
-    short queryClusterSupport(String clusterType) {
-        if (clusterType.equals(PROPERTY)) return READWRITE;
-        else                              return NONE;
+    short queryClusterSupport(String type) {
+        if (ClusterType.getValue(type) == ClusterType.PROPERTY) return READWRITE;
+        else                                          return NONE;
     }
 
     @Override
