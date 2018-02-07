c6c35a20266b6d62bcee3c568bda3923b72fe75c
Zsolt Kovacs <zs.myth@gmail.com>
2018-02-07 17:17:25 +0100

    use List<Property> instead of PropertyArrayList - #11

--- lookup-ldap/src/main/java/org/cristalise/lookup/ldap/LDAPProperties.java
+++ lookup-ldap/src/main/java/org/cristalise/lookup/ldap/LDAPProperties.java
@@ -22,9 +22,9 @@ package org.cristalise.lookup.ldap;
 
 /**
  * Directory Lookup Service
- * 
- * This represent 
- * 
+ *
+ * This represent
+ *
  */
 
 import javax.xml.bind.DatatypeConverter;
@@ -34,7 +34,7 @@ import org.cristalise.kernel.utils.ObjectProperties;
 
 /**
  * example:
- * 
+ *
  * <pre>
  * # LDAP Lookup config
  * # use the ApacheDS 2.0.0 M15 available using the port 10389
@@ -48,171 +48,176 @@ import org.cristalise.kernel.utils.ObjectProperties;
  * or
  * LDAP.password64=xxxxxx
  * </pre>
- * 
+ *
  * @version $Revision: 1.16 $ $Date: 2005/10/12 12:51:54 $
  * @author $Author: abranson $
  * @author ogattaz
  */
 public class LDAPProperties {
 
-	private static final String BAD_PASSWORD_MESSAGE = "bad base64 password value";
-	
-	public static final String LDAP_PROP_GPATH = "LDAP.GlobalPath";
-	public static final String LDAP_PROP_HOST = "LDAP.host";
-	public static final String LDAP_PROP_LPATH = "LDAP.LocalPath";
-	public static final String LDAP_PROP_PASS = "LDAP.password";
-	public static final String LDAP_PROP_PASS64 = "LDAP.password64";
-	public static final String LDAP_PROP_PORT = "LDAP.port";
-	public static final String LDAP_PROP_RPATH = "LDAP.RootPath";
-	public static final String LDAP_PROP_USER = "LDAP.user";
-	public static final String LDAP_PROP_TIMEOUT = "LDAP.timeOut";
-	public static final String LDAP_PROP_USETLS = "LDAP.useTLS";
-	public static final String LDAP_PROP_IGNORECERTERRORS = "LDAP.ignoreCertErrors";
-	
-	public String mGlobalPath = null; // o=cern.ch
-	public String mHost = null;
-	public String mLocalPath = null; // cn=lab27
-	public String mPassword = null;
-	public Integer mPort = null;
-	public String mRootPath = null; // cn=cristal2
-	public String mUser = null;
-	public Integer mTimeOut = null;
-	public boolean mUseTLS;
-	public boolean mIgnoreCertErrors;
+    private static final String BAD_PASSWORD_MESSAGE = "bad base64 password value";
 
-	/**
-	 * @param aObjectProps
-	 *            an ObjectProperties instance comming from clc file for exemple
-	 */
-	public LDAPProperties(final ObjectProperties aObjectProps) {
+    public static final String LDAP_PROP_GPATH = "LDAP.GlobalPath";
+    public static final String LDAP_PROP_HOST = "LDAP.host";
+    public static final String LDAP_PROP_LPATH = "LDAP.LocalPath";
+    public static final String LDAP_PROP_PASS = "LDAP.password";
+    public static final String LDAP_PROP_PASS64 = "LDAP.password64";
+    public static final String LDAP_PROP_PORT = "LDAP.port";
+    public static final String LDAP_PROP_RPATH = "LDAP.RootPath";
+    public static final String LDAP_PROP_USER = "LDAP.user";
+    public static final String LDAP_PROP_TIMEOUT = "LDAP.timeOut";
+    public static final String LDAP_PROP_USETLS = "LDAP.useTLS";
+    public static final String LDAP_PROP_IGNORECERTERRORS = "LDAP.ignoreCertErrors";
+    public static final String LDAP_PROP_ENABLEPAGINGMETHODS = "LDAP.enablePagingMethods";
 
-		if (aObjectProps != null) {
+    public String mGlobalPath = null; // o=cern.ch
+    public String mHost = null;
+    public String mLocalPath = null; // cn=lab27
+    public String mPassword = null;
+    public Integer mPort = null;
+    public String mRootPath = null; // cn=cristal2
+    public String mUser = null;
+    public Integer mTimeOut = null;
+    public boolean mUseTLS;
+    public boolean mIgnoreCertErrors;
 
-			mGlobalPath = aObjectProps.getProperty(LDAP_PROP_GPATH);
+    public boolean mEnablePagingMethods;
 
-			mRootPath = aObjectProps.getProperty(LDAP_PROP_RPATH);
-			if (mRootPath != null) {
-				mRootPath += "," + mGlobalPath;
-			}
+    /**
+     * @param aObjectProps
+     *            an ObjectProperties instance comming from clc file for exemple
+     */
+    public LDAPProperties(final ObjectProperties aObjectProps) {
 
-			mLocalPath = aObjectProps.getProperty(LDAP_PROP_LPATH);
-			if (mLocalPath != null) {
-				mLocalPath += "," + mRootPath;
-			}
+        if (aObjectProps != null) {
 
-			mPort = aObjectProps.getInt(LDAP_PROP_PORT, 389);
-			mHost = aObjectProps.getProperty(LDAP_PROP_HOST);
-			mTimeOut = aObjectProps.getInt(LDAP_PROP_TIMEOUT, 0);
-			mUser = aObjectProps.getProperty(LDAP_PROP_USER);
-			mPassword = aObjectProps.getProperty(LDAP_PROP_PASS);
-			mUseTLS = aObjectProps.getBoolean(LDAP_PROP_USETLS, false);
-			mIgnoreCertErrors = aObjectProps.getBoolean(LDAP_PROP_IGNORECERTERRORS, false);
+            mGlobalPath = aObjectProps.getProperty(LDAP_PROP_GPATH);
 
-			// if raw password not available, try to find base64 one
-			if (mPassword == null) {
-				mPassword = aObjectProps.getProperty(LDAP_PROP_PASS64);
-				// if base64 password available
-				if (mPassword != null) {
-					mPassword = translateBase64OPassword(mPassword);
-				}
-			}
-		}
-	}
+            mRootPath = aObjectProps.getProperty(LDAP_PROP_RPATH);
+            if (mRootPath != null) {
+                mRootPath += "," + mGlobalPath;
+            }
 
-	/**
-	 * @param aPropertyName
-	 *            the name of the property associated to the member
-	 * @param aMemberValue
-	 *            the value to check
-	 * @return true if valid
-	 * @throws IllegalArgumentException
-	 *             if not valid
-	 */
-	private boolean checkMemberValidity(final String aPropertyName,
-			final String aMemberValue) throws IllegalArgumentException {
+            mLocalPath = aObjectProps.getProperty(LDAP_PROP_LPATH);
+            if (mLocalPath != null) {
+                mLocalPath += "," + mRootPath;
+            }
 
-		if (isMemberValueValid(aMemberValue))
-			return true;
+            mPort = aObjectProps.getInt(LDAP_PROP_PORT, 389);
+            mHost = aObjectProps.getProperty(LDAP_PROP_HOST);
+            mTimeOut = aObjectProps.getInt(LDAP_PROP_TIMEOUT, 0);
+            mUser = aObjectProps.getProperty(LDAP_PROP_USER);
+            mPassword = aObjectProps.getProperty(LDAP_PROP_PASS);
+            mUseTLS = aObjectProps.getBoolean(LDAP_PROP_USETLS, false);
+            mIgnoreCertErrors = aObjectProps.getBoolean(LDAP_PROP_IGNORECERTERRORS, false);
 
-		throw new IllegalArgumentException(String.format(
-				"The LDAP property [%s] is not valid. The member value=[%s]",
-				aPropertyName, aMemberValue));
-	}
+            // if raw password not available, try to find base64 one
+            if (mPassword == null) {
+                mPassword = aObjectProps.getProperty(LDAP_PROP_PASS64);
+                // if base64 password available
+                if (mPassword != null) {
+                    mPassword = translateBase64OPassword(mPassword);
+                }
+            }
 
-	/**
-	 * @param aValue
-	 *            the value to be checked
-	 * @return true if not null and not empty
-	 */
-	private boolean checkPasswordValidity(final String aPasswordValue)
-			throws IllegalArgumentException {
+            mEnablePagingMethods = aObjectProps.getBoolean(LDAP_PROP_ENABLEPAGINGMETHODS, false);
+        }
+    }
 
-		if (checkMemberValidity(LDAP_PROP_PASS, aPasswordValue)) {
+    /**
+     * @param aPropertyName
+     *            the name of the property associated to the member
+     * @param aMemberValue
+     *            the value to check
+     * @return true if valid
+     * @throws IllegalArgumentException
+     *             if not valid
+     */
+    private boolean checkMemberValidity(final String aPropertyName,
+            final String aMemberValue) throws IllegalArgumentException {
 
-			if (aPasswordValue.contains(BAD_PASSWORD_MESSAGE)) {
-				throw new IllegalArgumentException(
-						String.format(
-								"The LDAP property [%s] is not valid. The member value=[%s]",
-								LDAP_PROP_PASS, aPasswordValue));
-			}
-		}
-		return true;
-	}
+        if (isMemberValueValid(aMemberValue))
+            return true;
 
-	/**
-	 * @return true is valid
-	 * @throws IllegalArgumentException
-	 *             if one of the members is not valid (null or empty)
-	 */
-	public boolean checkValidity() throws IllegalArgumentException {
+        throw new IllegalArgumentException(String.format(
+                "The LDAP property [%s] is not valid. The member value=[%s]",
+                aPropertyName, aMemberValue));
+    }
 
-		return checkMemberValidity(LDAP_PROP_GPATH, mGlobalPath)
-				&& checkMemberValidity(LDAP_PROP_RPATH, mRootPath)
-				&& checkMemberValidity(LDAP_PROP_LPATH, mLocalPath)
-				&& checkMemberValidity(LDAP_PROP_HOST, mHost)
-				&& checkMemberValidity(LDAP_PROP_USER, mUser)
-				&& checkPasswordValidity(mPassword);
-	}
+    /**
+     * @param aValue
+     *            the value to be checked
+     * @return true if not null and not empty
+     */
+    private boolean checkPasswordValidity(final String aPasswordValue)
+            throws IllegalArgumentException {
 
-	/**
-	 * @param aValue
-	 *            the value to be checked
-	 * @return true if not null and not empty
-	 */
-	private boolean isMemberValueValid(final String aValue) {
+        if (checkMemberValidity(LDAP_PROP_PASS, aPasswordValue)) {
 
-		return (aValue != null && !aValue.isEmpty());
-	}
+            if (aPasswordValue.contains(BAD_PASSWORD_MESSAGE)) {
+                throw new IllegalArgumentException(
+                        String.format(
+                                "The LDAP property [%s] is not valid. The member value=[%s]",
+                                LDAP_PROP_PASS, aPasswordValue));
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * @return true if the password is not null, not empty and is decoded id the
-	 *         passed property is a password64 one
-	 */
-	public boolean isPasswordValid() {
-		try {
-			return checkPasswordValidity(mPassword);
-		} catch (IllegalArgumentException ex) {
-			return false;
-		}
-	}
+    /**
+     * @return true is valid
+     * @throws IllegalArgumentException
+     *             if one of the members is not valid (null or empty)
+     */
+    public boolean checkValidity() throws IllegalArgumentException {
 
-	/**
-	 * @param aBase6Password
-	 *            the encoded password
-	 * @return the decodded password or a dummy phrase which cause an explicit
-	 *         error when it will be used during the connection
-	 */
-	private String translateBase64OPassword(final String aBase6Password) {
+        return checkMemberValidity(LDAP_PROP_GPATH, mGlobalPath)
+                && checkMemberValidity(LDAP_PROP_RPATH, mRootPath)
+                && checkMemberValidity(LDAP_PROP_LPATH, mLocalPath)
+                && checkMemberValidity(LDAP_PROP_HOST, mHost)
+                && checkMemberValidity(LDAP_PROP_USER, mUser)
+                && checkPasswordValidity(mPassword);
+    }
 
-		try {
-			// DatatypeConverter tool class available since java 1.5.
-			// Throws IllegalArgumentException if value not conform
-			return new String(
-					DatatypeConverter.parseBase64Binary(aBase6Password));
+    /**
+     * @param aValue
+     *            the value to be checked
+     * @return true if not null and not empty
+     */
+    private boolean isMemberValueValid(final String aValue) {
 
-		} catch (IllegalArgumentException ex) {
-			return String.format("#### %s [%s] ####", BAD_PASSWORD_MESSAGE,
-					aBase6Password);
-		}
-	}
+        return (aValue != null && !aValue.isEmpty());
+    }
+
+    /**
+     * @return true if the password is not null, not empty and is decoded id the
+     *         passed property is a password64 one
+     */
+    public boolean isPasswordValid() {
+        try {
+            return checkPasswordValidity(mPassword);
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * @param aBase6Password
+     *            the encoded password
+     * @return the decodded password or a dummy phrase which cause an explicit
+     *         error when it will be used during the connection
+     */
+    private String translateBase64OPassword(final String aBase6Password) {
+
+        try {
+            // DatatypeConverter tool class available since java 1.5.
+            // Throws IllegalArgumentException if value not conform
+            return new String(
+                    DatatypeConverter.parseBase64Binary(aBase6Password));
+
+        } catch (IllegalArgumentException ex) {
+            return String.format("#### %s [%s] ####", BAD_PASSWORD_MESSAGE,
+                    aBase6Password);
+        }
+    }
 }
