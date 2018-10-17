e17cf00a894c3ab99983fc62ad9cace3335ad8d8
Zsolt Kovacs <zs.myth@gmail.com>
2018-10-17 17:13:44 +0200

    #12 - add ModuleScriptBase class

---
+++ dsl/src/main/groovy/org/cristalise/dsl/module/ModuleScriptBase.groovy
@@ -0,0 +1,47 @@
+package org.cristalise.dsl.module
+
+import org.cristalise.kernel.common.InvalidDataException
+import org.cristalise.kernel.process.AbstractMain
+import org.cristalise.kernel.process.Gateway
+import org.cristalise.kernel.utils.Logger
+
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+
+@CompileStatic
+abstract class ModuleScriptBase extends Script {
+
+    private static final String defaultConnect = 'local.clc'
+    private static final String defaultConfig  = 'client.conf'
+
+    String configDir = null
+    String connect = null
+    String config = null
+
+    Integer logLevel = 0
+
+    private void init() {
+        Logger.addLogStream(System.out, logLevel)
+
+        if (configDir && (connect || config)) throw new InvalidDataException('Specify only configDir or connect/config')
+
+        if (configDir) {
+            config  = config  ?: "$configDir/$defaultConfig"
+            connect = connect ?: "$configDir/$defaultConnect"
+
+            Logger.msg(5, 'DslScriptBase - config:%s, connect:%s', config, connect)
+        }
+
+        if (!connect || !config) throw new InvalidDataException("Missing connect '"+connect+"' or config '"+config+"' files")
+
+        Gateway.init(AbstractMain.readPropertyFiles(config, connect, null))
+    }
+
+    public void Module(Map args, Closure cl) {
+        init()
+
+        ModuleBuilder.build((String)args.ns, (String)args.name, (Integer)args.version, cl)
+
+        Gateway.close()
+    }
+}
