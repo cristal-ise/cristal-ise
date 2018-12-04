2b899d127c39e73c755332f87b722e203809be57
Zsolt Kovacs <zs.myth@gmail.com>
2018-12-04 17:52:10 +0100

    #31: extend test to run the generated Module.groovy DSL file

--- dsl/src/test/groovy/org/cristalise/dsl/test/scaffold/CRUDGeneratorTest.groovy
+++ dsl/src/test/groovy/org/cristalise/dsl/test/scaffold/CRUDGeneratorTest.groovy
@@ -1,5 +1,6 @@
 package org.cristalise.dsl.test.scaffold
 
+import org.codehaus.groovy.control.CompilerConfiguration
 import org.cristalise.dsl.scaffold.CRUDGenerator
 import org.junit.Test
 
@@ -9,7 +10,14 @@ class CRUDGeneratorTest {
     void generateTestItem( ) {
         def vars = [rootDir: 'src/test', moduleName: 'Test Module', moduleNs: 'testns', moduleVersion: 0, item: 'TestItem']
         new CRUDGenerator().generate(vars, true, true)
-        
-        
+
+        CompilerConfiguration cc = new CompilerConfiguration()
+        cc.setScriptBaseClass(DelegatingScript.class.getName())
+
+        GroovyShell shell = new GroovyShell(this.class.classLoader, new Binding(), cc)
+        DelegatingScript script = (DelegatingScript) shell.parse(new File('src/test/module/Module.groovy'))
+
+        script.setDelegate(this)
+        script.run()
     }
 }
