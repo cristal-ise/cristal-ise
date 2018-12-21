e28d5874b35d581f65c6138763ad4ec65440caf5
Zsolt Kovacs <zs.myth@gmail.com>
2018-12-21 11:22:31 +0100

    #5: Wf execution test with permissions

--- testing/src/main/groovy/org/cristalise/dev/test/scenario/ShiroPermissionTestIT.groovy
+++ testing/src/main/groovy/org/cristalise/dev/test/scenario/ShiroPermissionTestIT.groovy
@@ -1,5 +1,6 @@
 package org.cristalise.dev.test.scenario
 
+import org.cristalise.kernel.lookup.AgentPath
 import org.cristalise.kernel.test.KernelScenarioTestBase
 import org.junit.Test
 
@@ -37,7 +38,64 @@ class ShiroPermissionTestIT extends KernelScenarioTestBase {
         
         def dummyItemP = agent.getItem(dummyItem.getItemPath())
 
-        dummyItemP.getJobList(oper1.getItemPath(),  true).size() == 2;
-        dummyItemP.getJobList(clerk1.getItemPath(), true).size() == 0;
+        assert dummyItemP.getJobList(oper1.getItemPath(),  true).size() == 2;
+        assert dummyItemP.getJobList(clerk1.getItemPath(), true).size() == 0;
+    }
+
+    @Test
+    public void 'Agent can execute activities with the proper Permission'() {
+        def oper1 = (AgentPath)Agent("oper1-$timeStamp") {
+            Roles {
+                Role(name: 'oper') {
+                    Permission('test:first,left,last')
+                }
+            }
+        }.getItemPath()
+
+        def clerk1 = (AgentPath)Agent("clerk1-$timeStamp") {
+            Roles {
+                Role(name: 'clerk') {
+                    Permission('test:right,last')
+                }
+            }
+        }.getItemPath()
+
+        def dummy = Item(name: "dummyItem-$timeStamp", folder: "testing") {
+            Property(Type: 'test')
+            Workflow {
+                EA('first')
+                AndSplit {
+                   Block { ElemAct("left") }
+                   Block { ElemAct("right") }
+               }
+               EA('last')
+            }
+        }
+
+        def dummyItem = agent.getItem(dummy.getItemPath())
+
+        checkJobs(dummyItem, oper1, [[stepName: "first", agentRole: null, transitionName: "Start"],
+                                     [stepName: "first", agentRole: null, transitionName: "Done"]])
+        checkJobs(dummyItem, clerk1, [])
+
+        def oper1Job = dummyItem.getJobByName('first', oper1)
+        assert oper1Job
+        def clerk1Job = dummyItem.getJobByName('first', clerk1)
+        assert !clerk1Job
+        dummyItem.requestAction(oper1Job)
+
+        assert dummyItem.getJobList(oper1,  true).size() == 2;
+        assert dummyItem.getJobList(clerk1, true).size() == 2;
+
+        oper1Job = dummyItem.getJobByName('left', oper1)
+        assert oper1Job
+        dummyItem.requestAction(oper1Job)
+
+        clerk1Job = dummyItem.getJobByName('right', clerk1)
+        assert clerk1Job
+        dummyItem.requestAction(clerk1Job)
+
+        assert dummyItem.getJobList(oper1,  true).size() == 2;
+        assert dummyItem.getJobList(clerk1, true).size() == 2;
     }
 }
