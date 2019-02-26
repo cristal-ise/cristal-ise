d5b7773a0891f85250b22cbe8350d11f2cad922a
Zsolt Kovacs <zs.myth@gmail.com>
2019-02-26 15:39:42 +0100

    #127: test generation of decimal mask

--- dsl/src/test/groovy/org/cristalise/dsl/test/persistency/outcome/SchemaBuilderDynymicFormsSpecs.groovy
+++ dsl/src/test/groovy/org/cristalise/dsl/test/persistency/outcome/SchemaBuilderDynymicFormsSpecs.groovy
@@ -42,25 +42,35 @@ class SchemaBuilderDynymicFormsSpecs extends Specification implements CristalTes
         expect:
         SchemaTestBuilder.build('test', 'PatientDetails', 0) {
             struct(name: 'PatientDetails') {
-                field(name: 'DateOfBirth', type: 'date') {dynamicForms(mask : '99/99/9999')}
+                field(name: 'Weight',      type: 'decimal') {dynamicForms(mask : '999.99')}
+                field(name: 'DateOfBirth', type: 'date')    {dynamicForms(mask : '99/99/9999')}
             }
         }.compareXML(
             """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
                  <xs:element name="PatientDetails">
                    <xs:complexType>
-                   <xs:all minOccurs="0">
-                   <xs:element name='DateOfBirth' type='xs:date' minOccurs='1' maxOccurs='1'>
-                     <xs:annotation>
-                       <xs:appinfo>
-                         <dynamicForms>
-                           <mask>99/99/9999</mask>
-                         </dynamicForms>
-                       </xs:appinfo>
-                     </xs:annotation>
-                    </xs:element>
-                   </xs:all>
-                 </xs:complexType>
-               </xs:element>
-             </xs:schema>""")
+                     <xs:all minOccurs="0">
+                       <xs:element name='Weight' type='xs:decimal' minOccurs='1' maxOccurs='1'>
+                         <xs:annotation>
+                           <xs:appinfo>
+                             <dynamicForms>
+                               <mask>999.99</mask>
+                             </dynamicForms>
+                           </xs:appinfo>
+                         </xs:annotation>
+                       </xs:element>
+                       <xs:element name='DateOfBirth' type='xs:date' minOccurs='1' maxOccurs='1'>
+                         <xs:annotation>
+                           <xs:appinfo>
+                             <dynamicForms>
+                               <mask>99/99/9999</mask>
+                             </dynamicForms>
+                           </xs:appinfo>
+                         </xs:annotation>
+                       </xs:element>
+                     </xs:all>
+                   </xs:complexType>
+                 </xs:element>
+               </xs:schema>""")
     }
 }
