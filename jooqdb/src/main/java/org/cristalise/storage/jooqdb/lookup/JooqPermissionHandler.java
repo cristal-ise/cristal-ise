6e2560ec15bebcfa7065a467cdee84610ee27ca0
Zsolt Kovacs <zs.myth@gmail.com>
2019-03-07 12:29:40 +0100

    #50: make PERMISSION column is not nullable (mysql restriction)

--- jooqdb/src/main/java/org/cristalise/storage/jooqdb/lookup/JooqPermissionHandler.java
+++ jooqdb/src/main/java/org/cristalise/storage/jooqdb/lookup/JooqPermissionHandler.java
@@ -47,7 +47,7 @@ public class JooqPermissionHandler {
     public void createTables(DSLContext context) throws PersistencyException {
         context.createTableIfNotExists(ROLE_PERMISSION_TABLE)
         .column(ROLE_PATH,  JooqHandler.STRING_TYPE .nullable(false))
-        .column(PERMISSION, JooqHandler.STRING_TYPE .nullable(true))
+        .column(PERMISSION, JooqHandler.STRING_TYPE .nullable(false))
         .constraints(constraint("PK_"+ROLE_PERMISSION_TABLE).primaryKey(ROLE_PATH, PERMISSION))
         .execute();
     }
