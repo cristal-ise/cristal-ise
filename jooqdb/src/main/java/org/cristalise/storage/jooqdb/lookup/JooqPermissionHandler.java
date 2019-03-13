b4d4a1450ef70b428fcebb0d91f055087bf6724a
Zsolt Kovacs <zs.myth@gmail.com>
2019-03-13 21:36:44 +0100

    #150: add back PK constraint

--- jooqdb/src/main/java/org/cristalise/storage/jooqdb/lookup/JooqPermissionHandler.java
+++ jooqdb/src/main/java/org/cristalise/storage/jooqdb/lookup/JooqPermissionHandler.java
@@ -48,7 +48,7 @@ public class JooqPermissionHandler {
         context.createTableIfNotExists(ROLE_PERMISSION_TABLE)
         .column(ROLE_PATH,  JooqHandler.STRING_TYPE.nullable(false))
         .column(PERMISSION, JooqHandler.TEXT_TYPE  .nullable(true))
-        .constraints(constraint("PK_"+ROLE_PERMISSION_TABLE).primaryKey(ROLE_PATH))
+        .constraints(constraint("PK_"+ROLE_PERMISSION_TABLE).primaryKey(ROLE_PATH, PERMISSION))
         .execute();
     }
 
