98624ce330f8af697b812b0dc825b19cf8869a1b
Zsolt Kovacs <zs.myth@gmail.com>
2019-04-07 11:28:32 +0200

    #166 new test are added to capture the bug

--- testing/src/main/groovy/org/cristalise/kernel/test/lifecycle/instance/CAExecutionSpecs.groovy
+++ testing/src/main/groovy/org/cristalise/kernel/test/lifecycle/instance/CAExecutionSpecs.groovy
@@ -21,7 +21,10 @@ class CAExecutionSpecs extends Specification implements CristalTestSetup {
         util = new WorkflowTestBuilder()
     }
 
-    def cleanup() { cristalCleanup() }
+    def cleanup() {
+        println Gateway.getMarshaller().marshall(util.wf)
+        cristalCleanup()
+    }
 
     def 'Execute ElemAct using Done transition'() {
         given: "Workflow contaning single ElemAct"
@@ -63,33 +66,97 @@ class CAExecutionSpecs extends Specification implements CristalTestSetup {
 
         then: "first ElemAct state is Finished and second is still Waiting"
         util.checkActStatus('rootCA', [state: "Started",  active: true])
-        util.checkActStatus('first',   [state: "Finished", active: false])
-        util.checkActStatus('second',  [state: "Waiting",  active: true])
+        util.checkActStatus('first',  [state: "Finished", active: false])
+        util.checkActStatus('second', [state: "Waiting",  active: true])
 
         when: "requesting second ElemAct Done transition"
         util.requestAction('second', "Done")
 
         then: "first ElemAct state is Finished and second is Finished"
         util.checkActStatus('rootCA', [state: "Started",  active: true])
-        util.checkActStatus('first',   [state: "Finished", active: false])
-        util.checkActStatus('second',  [state: "Finished", active: true])
+        util.checkActStatus('first',  [state: "Finished", active: false])
+        util.checkActStatus('second', [state: "Finished", active: true])
     }
 
-    def 'Execute ElemcAct in CompAct using Done transition'() {
-        given: "Workflow contaning CompAct containig one ElemAct"
+    def 'CompAct is automatically finihed when all Activities in sequence are finished'() {
+        given: "Workflow contaning CompAct containig 2 ElemAct in a sequence"
         util.buildAndInitWf { 
             CompAct('ca') { 
                 ElemAct('first') 
+                ElemAct('second') 
             }
         }
 
         when: "requesting first ElemAct Done transition"
         util.requestAction('first', "Done")
 
+        then: "ElemAct 'first' and CompAct state is Started"
+        util.checkActStatus('rootCA', [state: "Started",  active: true])
+        util.checkActStatus('ca',     [state: "Started",  active: true])
+        util.checkActStatus('first',  [state: "Finished", active: false])
+        util.checkActStatus('second', [state: "Waiting",  active: true])
+
+        when: "requesting second ElemAct Done transition"
+        util.requestAction('second', "Done")
+
         then: "ElemAct and CompAct state is Finished"
         util.checkActStatus('rootCA', [state: "Started",  active: true])
         util.checkActStatus('ca',     [state: "Finished", active: true])
         util.checkActStatus('first',  [state: "Finished", active: false])
+        util.checkActStatus('second', [state: "Finished", active: false])
+    }
+
+    def 'CompAct is automatically finihed when all Activities in AndSplit are finished'() {
+        given: "Workflow contaning CompAct containig 2 ElemAct in AndSplit"
+        util.buildAndInitWf { 
+            CompAct('ca') {
+                AndSplit {
+                    Block { ElemAct('left')  }
+                    Block { ElemAct('right') }
+                }
+            }
+        }
+        util.checkActStatus('rootCA', [state: "Started",  active: true])
+        util.checkActStatus('ca',     [state: "Started",  active: true])
+        util.checkActStatus('left',   [state: "Waiting",  active: true])
+        util.checkActStatus('right',  [state: "Waiting",  active: true])
+
+        when: "requesting left ElemAct Done transition"
+        util.requestAction('left', "Done")
+
+        then: "ElemAct 'left' and CompAct state is Started"
+        util.checkActStatus('rootCA', [state: "Started",  active: true])
+        util.checkActStatus('ca',     [state: "Started",  active: true])
+        util.checkActStatus('left',   [state: "Finished", active: false])
+        util.checkActStatus('right',  [state: "Waiting",  active: true])
+
+        when: "requesting right ElemAct Done transition"
+        util.requestAction('right', "Done")
+
+        then: "ElemAct and CompAct state is Finished"
+        util.checkActStatus('rootCA', [state: "Started",  active: true])
+        util.checkActStatus('ca',     [state: "Finished", active: true])
+        util.checkActStatus('left',   [state: "Finished", active: false])
+        util.checkActStatus('right',  [state: "Finished", active: false])
+    }
+
+    def 'CompAct is automatically finihed when all Activities in Loop are finished'() {
+        given: "Workflow contaning CompAct containig 1 ElemAct in Loop"
+        util.buildAndInitWf {
+            CompAct('ca') {
+                Loop(RoutingScriptName: 'javascript:\"false\";') { //loop shall finish automatically
+                    ElemAct('one')
+                }
+            }
+        }
+
+        when: "requesting one ElemAct Done transition"
+        util.requestAction('one', "Done")
+
+        then: "ElemAct 'one' and CompAct state is Finished"
+        util.checkActStatus('rootCA', [state: "Started",  active: true])
+        util.checkActStatus('ca',     [state: "Finished", active: false])
+        util.checkActStatus('one',    [state: "Finished", active: false])
     }
 
     def 'Empty Wf cannot be executed'() {
@@ -129,10 +196,10 @@ class CAExecutionSpecs extends Specification implements CristalTestSetup {
     def 'Cannot complete Compact without finishing all ElemActs'() {
         given: "Workflow containing single CompAct with a single ElemAct"
         util.buildAndInitWf {
-			CompAct('ca') {
-				ElemAct('first') 
-			} 
-		}
+            CompAct('ca') {
+                ElemAct('first')
+            }
+        }
 
         when: "requesting CompAct Complete transition"
         util.requestAction('ca', "Complete")
@@ -140,23 +207,24 @@ class CAExecutionSpecs extends Specification implements CristalTestSetup {
         then: "InvalidTransitionException is thrown"
         thrown InvalidTransitionException
     }
-	
-	def 'Compact can be finished with active children if Abortable'() {
-		given: "Workflow containing single CompAct with a single ElemAct"
-		util.buildAndInitWf {CompAct('ca') {
-				Property('Abortable': true) 
-				ElemAct('first') 
-			} 
-		}
-		util.checkActStatus('ca',   [state: "Started", active: true])
-		
-		when: "requesting Root CompAct Complete transition"
-		util.requestAction('ca', "Complete")
 
-		then: "CompAct is finished, child is inactive but Waiting"
-		util.checkActStatus('ca',   [state: "Finished", active: true])
-		util.checkActStatus('first',   [state: "Waiting", active: false])
-	}
+    def 'Compact can be finished with active children if Abortable'() {
+        given: "Workflow containing single CompAct with a single ElemAct"
+        util.buildAndInitWf {
+            CompAct('ca') {
+                Property('Abortable': true)
+                ElemAct('first')
+            }
+        }
+        util.checkActStatus('ca',   [state: "Started", active: true])
+
+        when: "requesting Root CompAct Complete transition"
+        util.requestAction('ca', "Complete")
+
+        then: "CompAct is finished, child is inactive but Waiting"
+        util.checkActStatus('ca',   [state: "Finished", active: true])
+        util.checkActStatus('first',   [state: "Waiting", active: false])
+    }
 
     def 'Cannot Complete Root Compact without finishing all CompActs'() {
         given: "Workflow contaning single and empty CompAct"
@@ -168,5 +236,4 @@ class CAExecutionSpecs extends Specification implements CristalTestSetup {
         then: "InvalidTransitionException is thrown"
         thrown InvalidTransitionException
     }
-
 }
