86249201022db7438d22ac0e0f20b23b88d6c23f
Zsolt Kovacs <zs.myth@gmail.com>
2020-06-04 15:35:03 +0200

    #369 add test case to trigger the functionality

---
+++ testing/src/main/groovy/org/cristalise/restapi/test/AgentPredefinedStepsTest.groovy
@@ -0,0 +1,42 @@
+package org.cristalise.restapi.test
+
+import org.cristalise.kernel.lifecycle.instance.predefined.ChangeName
+import org.cristalise.kernel.lifecycle.instance.predefined.agent.Sign
+import org.junit.Test
+
+import groovy.transform.CompileStatic
+import io.restassured.http.ContentType
+
+@CompileStatic
+class AgentPredefinedStepsTest extends RestapiTestBase {
+
+    @Test
+    public void 'Sign can be used to create a SimpleElectronicSignature record'() {
+        def name = "TestAgent-$timeStamp"
+        login('user', 'test')
+        createNewAgent(name, 'test', ContentType.XML)
+        logout(null)
+
+        login(name, 'test')
+
+        StringBuffer xml = new StringBuffer("<SimpleElectonicSignature>");
+        xml.append("<AgentName>").append(name)      .append("</AgentName>");
+        xml.append("<Password>") .append("test1").append("</Password>");
+
+        xml.append("<ExecutionContext>");
+        xml.append("<ItemPath>")     .append("ItemPath")     .append("</ItemPath>");
+        xml.append("<SchemaName>")   .append("SchemaName")   .append("</SchemaName>");
+        xml.append("<SchemaVersion>").append("SchemaVersion").append("</SchemaVersion>");
+        xml.append("<ActivityType>") .append("ActivityType") .append("</ActivityType>");
+        xml.append("<ActivityName>") .append("ActivityName") .append("</ActivityName>");
+        xml.append("<StepPath>")     .append("StepPath")     .append("</StepPath>");
+        xml.append("</ExecutionContext>");
+
+        xml.append("</SimpleElectonicSignature>");
+        executePredefStep(userUuid, Sign.class, ContentType.XML, xml.toString())
+
+        //newAgent.getViewpoint('SimpleElectonicSignature', 'last')
+
+        logout(null)
+    }
+}
