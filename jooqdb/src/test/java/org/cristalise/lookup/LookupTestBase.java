19a7e408a758220e1904fcba39c60ab19661476a
Zsolt Kovács <zs.myth@gamil.com>
2020-09-28 09:16:49 +0200

    #426 jooqdb lookup test are fixed

--- jooqdb/src/test/java/org/cristalise/lookup/LookupTestBase.java
+++ jooqdb/src/test/java/org/cristalise/lookup/LookupTestBase.java
@@ -20,10 +20,6 @@
  */
 package org.cristalise.lookup;
 
-import static org.cristalise.JooqTestConfigurationBase.DBModes.MYSQL;
-import static org.cristalise.JooqTestConfigurationBase.DBModes.PostgreSQL;
-import static org.cristalise.JooqTestConfigurationBase.DBModes.H2_PostgreSQL;
-import static org.cristalise.JooqTestConfigurationBase.DBModes.H2_MYSQL;
 import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
 import static org.unitils.reflectionassert.ReflectionComparatorMode.LENIENT_ORDER;
 
@@ -73,8 +69,11 @@ public class LookupTestBase extends JooqTestConfigurationBase {
 
     @After
     public void tearDown() throws Exception {
-        if (lookup != null) lookup.close();
-        if (dbType == MYSQL || dbType == PostgreSQL || dbType == H2_PostgreSQL || dbType == H2_MYSQL) lookup.dropHandlers();
+        //if (dbType == MYSQL || dbType == PostgreSQL || dbType == H2_PostgreSQL || dbType == H2_MYSQL) lookup.dropHandlers();
+        if (lookup != null) {
+            lookup.dropHandlers();
+            lookup.close();
+        }
     }
 
     public void compare(List<Path> expecteds, Iterator<Path> actualsIter) {
