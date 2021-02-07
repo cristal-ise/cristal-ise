4b23adf4a83e32c6bd986f5f648d16b1ca6203af
Zsolt Kovacs <zs.myth@gmail.com>
2021-02-07 19:39:18 +0100

    #471 remove try/catch to expose error

--- dsl/src/main/groovy/org/cristalise/dsl/persistency/outcome/DSLPropertySetter.groovy
+++ dsl/src/main/groovy/org/cristalise/dsl/persistency/outcome/DSLPropertySetter.groovy
@@ -30,27 +30,24 @@ class DSLPropertySetter implements ChildPropertySetter {
 
     @Override
     public void setChild(Object parent, Object child, String parentName, String propertyName) {
-        try {
-            Object property = InvokerHelper.getProperty(parent, propertyName);
+        log.debug 'setChild({}, {})', parentName, propertyName
 
-            if (property != null) {
-                if (Collection.class.isAssignableFrom(property.getClass())) {
-                    ((Collection) property).add(child);
-                }
-                else if (Map.class.isAssignableFrom(property.getClass())) {
-                    ((Map) property).put(child.name, child);
-                    parent.orderOfElements.add(child.name)
-                }
-                else  {
-                    InvokerHelper.setProperty(parent, propertyName, child);
-                }
+        Object property = InvokerHelper.getProperty(parent, propertyName);
+
+        if (property != null) {
+            if (Collection.class.isAssignableFrom(property.getClass())) {
+                ((Collection) property).add(child);
             }
-            else {
+            else if (Map.class.isAssignableFrom(property.getClass())) {
+                ((Map) property).put(child.name, child);
+                parent.orderOfElements.add(child.name)
+            }
+            else  {
                 InvokerHelper.setProperty(parent, propertyName, child);
             }
         }
-        catch (MissingPropertyException mpe) {
-            log.warn "setChild($parentName, $propertyName) - MissingPropertyException:" + mpe.message
+        else {
+            InvokerHelper.setProperty(parent, propertyName, child);
         }
     }
 }
