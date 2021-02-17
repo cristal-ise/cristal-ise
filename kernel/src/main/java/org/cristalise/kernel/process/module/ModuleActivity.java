ff32addd38ff94a4dbbc95c80842faae4c83e7c2
Zsolt Kovács <zs.myth@gmail.com>
2021-02-17 11:26:07 +0100

    #472 ModuleActivity.create() check resourceChangeStatus

--- kernel/src/main/java/org/cristalise/kernel/process/module/ModuleActivity.java
+++ kernel/src/main/java/org/cristalise/kernel/process/module/ModuleActivity.java
@@ -86,6 +86,7 @@ public class ModuleActivity extends ModuleResource {
         try {
             ResourceImportHandler importHandler = Gateway.getResourceImportHandler(type);
             domainPath = importHandler.importResource(ns, name, version, itemPath, getResourceFileName(), reset, transactionKey);
+            resourceChangeStatus  = importHandler.getResourceChangeStatus();
             resourceChangeDetails = importHandler.getResourceChangeDetails();
             itemPath = domainPath.getItemPath(transactionKey);
         }
@@ -94,22 +95,40 @@ public class ModuleActivity extends ModuleResource {
             throw new CannotManageException("Exception verifying module resource " + ns + "/" + name);
         }
 
-        actDef = LocalObjectLoader.getActDef(name, version, transactionKey);
-        populateActivityDef(transactionKey);
+        switch (resourceChangeStatus) {
+            case NEW:
+            case UPDATED:
+            case OVERWRITTEN:
+                actDef = LocalObjectLoader.getActDef(name, version, transactionKey);
+                populateActivityDef(transactionKey);
 
-        CollectionArrayList colls = actDef.makeDescCollections(transactionKey);
+                CollectionArrayList colls = actDef.makeDescCollections(transactionKey);
 
-        for (Collection<?> coll : colls.list) {
-            try {
-                Gateway.getStorage().put(itemPath, coll, transactionKey);
-                // create last collection
-                coll.setVersion(null);
-                Gateway.getStorage().put(itemPath, coll, transactionKey);
-            }
-            catch (PersistencyException e) {
-                log.error("Storing description collections for " + getName() + ".", e);
-                throw new CannotManageException("Persistency exception storing description collections for " + getName() + ".");
-            }
+                for (Collection<?> coll : colls.list) {
+                    try {
+                        Gateway.getStorage().put(itemPath, coll, transactionKey);
+                        // create last collection
+                        coll.setVersion(null);
+                        Gateway.getStorage().put(itemPath, coll, transactionKey);
+                    }
+                    catch (PersistencyException e) {
+                        log.error("Storing description collections for " + getName() + ".", e);
+                        throw new CannotManageException("Persistency exception storing description collections for " + getName() + ".");
+                    }
+                }
+                break;
+
+            case IDENTICAL:
+            case SKIPPED:
+                //nothing to do
+                break;
+
+            case REMOVED:
+                // TODO implement
+                throw new CannotManageException("Unimplemented resourceChangeStatus '"+resourceChangeStatus+ "' for "+ns+"/"+name);
+
+            default:
+                throw new CannotManageException("Unknown resourceChangeStatus '"+resourceChangeStatus+ "' for "+ns+"/"+name);
         }
 
         return domainPath;
@@ -117,40 +136,55 @@ public class ModuleActivity extends ModuleResource {
 
     public void populateActivityDef(TransactionKey transactionKey) throws ObjectNotFoundException, CannotManageException {
         try {
-            if (schema != null)
-                actDef.setSchema(LocalObjectLoader.getSchema(schema.id == null ? schema.name : schema.id, Integer.valueOf(schema.version), transactionKey));
+            if (schema != null) {
+                actDef.setSchema(LocalObjectLoader.getSchema(
+                        schema.id == null ? schema.name : schema.id, 
+                        Integer.valueOf(schema.version), transactionKey));
+            }
         }
         catch (NumberFormatException | InvalidDataException e) {
-            log.error("", e);
-            throw new CannotManageException("Schema definition in " + getName() + " not valid.");
+            String msg = "Invalid Schema definition for " +ns+"/"+name;
+            log.error(msg, e);
+            throw new CannotManageException(msg + " exception:" + e.getMessage());
         }
 
         try {
-            if (script != null)
-                actDef.setScript(LocalObjectLoader.getScript(script.id == null ? script.name : script.id, Integer.valueOf(script.version), transactionKey));
+            if (script != null) {
+                actDef.setScript(LocalObjectLoader.getScript(
+                        script.id == null ? script.name : script.id,
+                        Integer.valueOf(script.version), transactionKey));
+            }
         }
         catch (NumberFormatException | InvalidDataException e) {
-            log.error("", e);
-            throw new CannotManageException("Script definition in " + getName() + " not valid.");
+            String msg = "Invalid Script definition for " +ns+"/"+name;
+            log.error(msg, e);
+            throw new CannotManageException(msg + " exception:" + e.getMessage());
         }
 
         try {
-            if (query != null)
-                actDef.setQuery(LocalObjectLoader.getQuery(query.id == null ? query.name : query.id, Integer.valueOf(query.version), transactionKey));
+            if (query != null) {
+                actDef.setQuery(LocalObjectLoader.getQuery(
+                        query.id == null ? query.name : query.id, 
+                        Integer.valueOf(query.version), transactionKey));
+            }
         }
         catch (NumberFormatException | InvalidDataException e) {
-            log.error("", e);
-            throw new CannotManageException("Query definition in " + getName() + " not valid.");
+            String msg = "Invalid Query definition for " +ns+"/"+name;
+            log.error(msg, e);
+            throw new CannotManageException(msg + " exception:" + e.getMessage());
         }
 
         try {
-            if (stateMachine != null)
-                actDef.setStateMachine(LocalObjectLoader.getStateMachine(stateMachine.id == null ? stateMachine.name : stateMachine.id,
+            if (stateMachine != null) {
+                actDef.setStateMachine(LocalObjectLoader.getStateMachine(
+                        stateMachine.id == null ? stateMachine.name : stateMachine.id,
                         Integer.valueOf(stateMachine.version), transactionKey));
+            }
         }
         catch (NumberFormatException | InvalidDataException e) {
-            log.error("", e);
-            throw new CannotManageException("State Machine definition in " + getName() + " not valid.");
+            String msg = "Invalid State Machine definition for " +ns+"/"+name;
+            log.error(msg, e);
+            throw new CannotManageException(msg + " exception:" + e.getMessage());
         }
     }
 }
