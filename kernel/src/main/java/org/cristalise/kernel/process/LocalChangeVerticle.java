ede97954b394881d7050da23a8e647e9862d1fec
Zsolt Kovacs <zs.myth@gmail.com>
2021-05-06 15:42:43 +0200

    #480 LocalChangeVerticle = CacheInvalidator + LocalMessage verticles

---
+++ kernel/src/main/java/org/cristalise/kernel/process/LocalChangeVerticle.java
@@ -0,0 +1,87 @@
+/**
+ * This file is part of the CRISTAL-iSE kernel.
+ * Copyright (c) 2001-2015 The CRISTAL Consortium. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
+package org.cristalise.kernel.process;
+
+import java.util.ArrayList;
+
+import org.cristalise.kernel.common.InvalidDataException;
+import org.cristalise.kernel.entity.proxy.ProxyMessage;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.json.JsonArray;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Subscribes to the ProxyMessages channel to perform 2 actions: cleans the local cache of ClusterStorage 
+ * and sends change notifications to local subscribers. The order is very important, the cache 
+ * should be cleared first so the notified consumer will read the new value.
+ */
+@Slf4j
+public class LocalChangeVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        vertx.eventBus().consumer(ProxyMessage.ebAddress, message -> {
+            Object body = message.body();
+            log.debug("handler() - message.body:{}", body);
+
+            try {
+                //order is important: see above
+                clearCache((JsonArray)body);
+                sendLocalMessages((JsonArray)body);
+            }
+            catch (Exception e) {
+                log.error("handler()", e);
+            }
+        });
+
+        startPromise.complete();
+        log.info("start() - '{}' consumer configured", ProxyMessage.ebAddress);
+    }
+
+    private void sendLocalMessages(JsonArray body) throws InvalidDataException {
+        DeliveryOptions opt = new DeliveryOptions().setLocalOnly(true);
+
+        for (Object element: body) {
+            ProxyMessage msg = new ProxyMessage((String)element);
+
+            log.trace("handler() - sending address:{}, msg:{}", msg.getLocalEventBusAddress(), msg.getLocalEventBusMessage());
+            vertx.eventBus().publish(msg.getLocalEventBusAddress(), msg.getLocalEventBusMessage(), opt);
+        }
+    }
+
+    private void clearCache(JsonArray body) throws InvalidDataException {
+        ArrayList<String> clearCacheList = new ArrayList<String>();
+
+        for (Object element: body) {
+            ProxyMessage msg = new ProxyMessage((String)element);
+
+            if (msg.isClusterStoreMesssage()) {
+                clearCacheList.add(msg.getItemPath().getUUID() + "/" + msg.getPath());
+            }
+        }
+
+        log.trace("clearCache() - invalidating #{} cache entries", clearCacheList.size());
+        Gateway.getStorage().clearCache(clearCacheList);
+    }
+}
--- kernel/src/main/java/org/cristalise/kernel/process/StandardClient.java
+++ kernel/src/main/java/org/cristalise/kernel/process/StandardClient.java
@@ -26,11 +26,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.cristalise.kernel.common.InvalidDataException;
 import org.cristalise.kernel.entity.proxy.AgentProxy;
 import org.cristalise.kernel.lifecycle.instance.stateMachine.StateMachine;
-import org.cristalise.kernel.persistency.CacheInvalidatorVerticle;
 import org.cristalise.kernel.process.resource.ResourceLoader;
 import org.cristalise.kernel.utils.LocalObjectLoader;
 
-import io.vertx.core.DeploymentOptions;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -41,10 +39,8 @@ abstract public class StandardClient extends AbstractMain {
      * 
      */
      public static void createClientVerticles() {
-        int ivInstances = Gateway.getProperties().getInt("ProxyMessageVerticle.instances", 1);
-        DeploymentOptions options = new DeploymentOptions().setInstances(ivInstances);
-
-        Gateway.getVertx().deployVerticle(CacheInvalidatorVerticle.class, options);
+         //deploys only one such verticles per client process
+        Gateway.getVertx().deployVerticle(new LocalChangeVerticle());
     }
 
     /**
