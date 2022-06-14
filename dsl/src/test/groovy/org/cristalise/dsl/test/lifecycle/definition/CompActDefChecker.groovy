44028173a33f795ed707ed7f0d6d022d0c70cab2
Zsolt Kovacs <zs.myth@gmail.com>
2022-06-14 09:58:34 +0200

    Fix compilation error in maven #422

--- dsl/src/test/groovy/org/cristalise/dsl/test/lifecycle/definition/CompActDefChecker.groovy
+++ dsl/src/test/groovy/org/cristalise/dsl/test/lifecycle/definition/CompActDefChecker.groovy
@@ -1,5 +1,24 @@
+/**
+ * This file is part of the CRISTAL-iSE kernel.
+ * Copyright (c) 2001-2015 The CRISTAL Consortium. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
 package org.cristalise.dsl.test.lifecycle.definition
-
 
 import org.cristalise.kernel.graph.model.DirectedEdge
 import org.cristalise.kernel.graph.model.Vertex
@@ -27,7 +46,7 @@ class CompActDefChecker {
 
     private Vertex getOutVertex(Vertex currentVertex, Object sequenceData) {
         def verticles = caDef.childrenGraphModel.getOutVertices(currentVertex)
-        def foundVertex = null
+        Vertex foundVertex = null
 
         if (sequenceData instanceof String) {
             foundVertex = verticles.find { it.name == sequenceData }
