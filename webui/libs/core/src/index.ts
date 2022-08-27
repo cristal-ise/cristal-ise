7462956abdf60e2c87f775f05bc136a7a683fbe3
Zsolt Kovács <zs.myth@gmail.com>
2022-08-27 20:59:53 +0200

    #596 add Logger to core library

--- webui/libs/core/src/index.ts
+++ webui/libs/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/core.module';
 export * from './lib/guards/authentication.guard';
 export * from './lib/services/cookie-authentication.service';
+export * from './lib/api/logger'
