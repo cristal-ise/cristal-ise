acbb2c0ee91768fcca7c28e7a5feaac2b5d2e3df
Zsolt Kovács <zs.myth@gmail.com>
2022-08-28 10:05:28 +0200

    #596 reimplement CookieAuthenticationService.login()

--- webui/libs/core/src/lib/services/cookie-authentication.service.ts
+++ webui/libs/core/src/lib/services/cookie-authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-// import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
+import { Logger } from '@cristalise/core';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,45 @@ export class CookieAuthenticationService {
 
   private loggedInUser = new Subject<string>();
 
-  login(userName: string, password: string): void {
-    if (userName && password) {
-      this.loggedInUser.next(userName);
-      this.isLoggedIn = true;
-    }
+  private root = "/api";
+
+  private log = new Logger('@cristalise/core', 'CookieAuthenticationService')
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(userName: string, password: string): Observable<boolean> {
+    const result = new Subject<boolean>();
+
+    this.callLoginPost(userName, password).subscribe({
+      next: (value) => {
+        this.log.debug('login()', value);
+        if (value['Login'] && value['Login'].uuid) {
+          this.loggedInUser.next(value['Login'].uuid);
+          this.isLoggedIn = true;
+          result.next(true);
+        }
+        else {
+          result.next(false);
+        }
+      },
+      error: (error) => {
+        this.log.debug('login()', error);
+        result.next(false);
+      },
+      complete: () => {
+        this.log.debug('login()', 'complete');
+      }
+    });
+    return result.asObservable();
+  }
+
+  callLoginPost(agent: string, pwd: string): Observable<any> {
+    const url = this.root + '/login';
+    // btoa encrypts username and password to base64 - UTF8 Charset
+    const body = JSON.stringify({ username: btoa(agent), password: btoa(pwd) });
+
+    return this.http.post(url, body, { withCredentials: true });
   }
 
   logout(): void {
