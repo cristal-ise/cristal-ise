63ecb841ba7450ae794300e62bbc563d6d1ca0dd
Zsolt Kovács <zs.myth@gmail.com>
2022-09-09 10:06:19 +0200

    #601 code reformat

--- webui/apps/admin/src/app/layout/menu/app.menu.component.ts
+++ webui/apps/admin/src/app/layout/menu/app.menu.component.ts
@@ -4,31 +4,16 @@ import { MenuItem } from 'primeng/api';
 import { LayoutService } from '../service/app.layout.service';
 
 @Component({
-    selector: 'cristalise-menu',
-    templateUrl: './app.menu.component.html'
+  selector: 'cristalise-menu',
+  templateUrl: './app.menu.component.html'
 })
 export class AppMenuComponent implements OnInit {
 
-    model: MenuItem[] = [];
+  model: MenuItem[] = [];
 
-    constructor(public layoutService: LayoutService) { }
+  constructor(public layoutService: LayoutService) { }
 
-    ngOnInit() {
-        this.model = [
-            {
-                label: 'Home',
-                items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
-                ]
-            },
-            {
-                label: 'Descriptions',
-                items: []
-            },
-            {
-                label: 'Objects',
-                items: []
-            }
-        ];
-    }
+  ngOnInit() {
+    this.model = [];
+  }
 }
