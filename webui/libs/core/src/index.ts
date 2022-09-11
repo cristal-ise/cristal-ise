563151f2a6684f7f2393c40830f71dcb1efeb383
Zsolt Kovács <zs.myth@gmail.com>
2022-09-11 12:16:49 +0200

    #601 Lookup service getDomainTree()

--- webui/libs/core/src/index.ts
+++ webui/libs/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/core.module';
+export * from './lib/api/logger'
 export * from './lib/guards/authentication.guard';
 export * from './lib/services/cookie-authentication.service';
-export * from './lib/api/logger'
+export * from './lib/services/lookup.service';
