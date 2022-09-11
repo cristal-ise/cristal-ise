d31744fcbe0b79cae8c7a471986b21d45b394508
Zsolt Kovács <zs.myth@gmail.com>
2022-09-11 12:16:06 +0200

    #601 Logger issue fixed

--- webui/libs/core/src/lib/api/logger.ts
+++ webui/libs/core/src/lib/api/logger.ts
@@ -29,27 +29,22 @@ export class Logger {
     this.log(LogLevel.TRACE, args)
   }
 
-  private log(msgLevel: LogLevel, ...args: any[]): void {
+  private log(msgLevel: LogLevel, args: any[]): void {
     if (this.configLevel < msgLevel) {
       return
     }
 
-    /* TODO: make this work
     // Extend prefix with method name if it was provided in the first paramater
     if (typeof args[0] === 'string' && args[0].toString().endsWith('()')) {
       const method: string = args.shift();
       this.prefix += '.' + method;
     }
 
-    // eslint-disable-next-line no-restricted-syntax
-    console.debug('Logger', 'prefix:', this.prefix, 'configLevel:', this.configLevel, 'msgLevel:', msgLevel)
-    */
-
     if (msgLevel === LogLevel.ERROR) {
-      console.error(this.prefix, ...args);
+      console.error(this.prefix, args);
     }
     else if (msgLevel === LogLevel.WARNING) {
-      console.warn(this.prefix, ...args);
+      console.warn(this.prefix, args);
     }
     else if (msgLevel === LogLevel.INFO) {
       console.log(this.prefix, ...args);
