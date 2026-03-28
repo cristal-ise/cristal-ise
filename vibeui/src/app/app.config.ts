f60924b193f99e1058c31fd70947a3cebdaa82dc
Zsolt Kovacs <zs.myth@gmail.com>
2026-03-28 10:23:48 +0100

    #704 - Implemented CRISTAL-iSE OpenAPI services

--- vibeui/src/app/app.config.ts
+++ vibeui/src/app/app.config.ts
@@ -1,15 +1,20 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { provideApi } from './api/provide-api';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
+    provideApi(environment.apiUrl),
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
